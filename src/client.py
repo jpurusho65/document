656ef5568bd06e88e6c21914bc3303be9f727580
Jerome Purushotham <jerome@aqueducthq.com>
2023-12-14 17:03:40 -0800

    Add some source files

---
+++ src/client.py
@@ -0,0 +1,13 @@
+import asyncio
+import websockets
+
+async def test_websocket():
+    uri = "ws://localhost:8000/ws"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("Start")
+        for i in range(5):  # Simulating periodic updates
+            response = await websocket.recv()
+            print(response)
+            await websocket.send(f"Update {i}")
+
+asyncio.run(test_websocket())
