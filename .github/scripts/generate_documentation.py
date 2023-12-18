b9aca6dd2bf1b7997499da926ebdfa47b6cdd6ee
GitHub Action <action@github.com>
2023-12-18 13:38:50 -0800

    Gen script updated

--- .github/scripts/generate_documentation.py
+++ .github/scripts/generate_documentation.py
@@ -1,6 +1,6 @@
 import os
-import json
 from openai import OpenAI
+import random
 
 import argparse
 from git import Repo
@@ -28,12 +28,33 @@ def generate_documentation(client, file_path):
                         )
     return response.choices[0].message.content
 
-def handle_changed_files(client, changed_files):
+def remove_code_block_formatting(original_content):
+    # Split the content into lines
+    lines = original_content.split('\n')
+
+    # Check if the first line starts with ```
+    if lines[0].strip().startswith("```"):
+        # Remove the first line (```language)
+        lines = lines[1:]
+
+    # Check if the last line ends with ```
+    if lines[-1].strip() == "```":
+        # Remove the last line (```)
+        lines = lines[:-1]
+
+    # Join the lines back into a single string
+    cleaned_content = '\n'.join(lines)
+
+    return cleaned_content
+
+
+def document_changed_files(client, changed_files):
     docs = {}
     for file_path in changed_files:
         if os.path.isfile(file_path):
-            docs[file_path] = generate_documentation(client, file_path)
+            docs[file_path] = remove_code_block_formatting(generate_documentation(client, file_path))
     return docs
+
 
 def git_commit(documentation):
     # Set up Git operations
@@ -41,7 +62,8 @@ def git_commit(documentation):
     git = repo.git
 
     # Create a new branch
-    new_branch = "docs-update"
+    b_id = random.randint(1, 1000)
+    new_branch = f"docs-update-{b_id}"
     git.checkout('HEAD', b=new_branch)
 
     # Commit and push documentation
@@ -67,5 +89,5 @@ if __name__ == "__main__":
     client = OpenAI()
     client.api_key=os.environ['OPENAI_API_KEY']
     changed_files = args.files
-    documentation = handle_changed_files(client, changed_files)
+    documentation = document_changed_files(client, changed_files)
     git_commit(documentation)
